8b440f21ad7d265884db8ae117736b5de7b74bf5
Brian Vallelunga <vallelungabrian@gmail.com>
2014-08-04 03:54:54 -0700

    Facebook auth working

--- Juicy/AppDelegate.swift
+++ Juicy/AppDelegate.swift
@@ -25,17 +25,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         Parse.setApplicationId(parseApplicationID, clientKey: parseClientKey)
         
         //Initialize Facebook
-        //PFFacebookUtils.initializeFacebook()
+        PFFacebookUtils.initializeFacebook()
         return true
     }
     
-//    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString, annotation: AnyObject) -> Bool {
-//            return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, withSession:PFFacebookUtils.session())
-//    }
-//    
-//    func applicationDidBecomeActive(application: UIApplication) {
-//        FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
-//    }
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: NSString, annotation: AnyObject) -> Bool {
+            return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication, withSession:PFFacebookUtils.session())
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
+    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Juicy/LandingViewController.swift
+++ Juicy/LandingViewController.swift
@@ -9,12 +9,30 @@
 import Foundation
 
 class LandingViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if PFUser.currentUser() {
             //self.performSegueWithIdentifier("loggedIn", sender: self)
         }
+    }
+    
+    @IBAction func connectButtonDown(sender: UIButton) {
+        sender.backgroundColor = UIColor(red: 0.14, green: 0.22, blue: 0.36, alpha: 1)
+    }
+    
+    @IBAction func connectButtonReleased(sender: UIButton) {
+        sender.backgroundColor = UIColor(red: 0.25, green: 0.37, blue: 0.58, alpha: 1)
+        
+        PFFacebookUtils.logInWithPermissions(nil, {
+            (user: PFUser!, error: NSError!) -> Void in
+            if !user {
+                NSLog("Uh oh. The user cancelled the Facebook login.")
+            } else if user.isNew {
+                NSLog("User signed up and logged in through Facebook!")
+            } else {
+                NSLog("User logged in through Facebook!")
+            }
+        })
     }
 }
