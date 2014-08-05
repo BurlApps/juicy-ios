be922126f8a0c784d6af332392e841a89c22c75d
Brian Vallelunga <vallelungabrian@gmail.com>
2014-08-04 20:41:40 -0700

    updated logos and added spinner to Facebook button

--- Juicy/LandingViewController.swift
+++ Juicy/LandingViewController.swift
@@ -9,8 +9,17 @@
 import Foundation
 
 class LandingViewController: UIViewController {
+    
+    var spinner: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
+    @IBOutlet weak var connectButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.spinner = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.White)
+        self.spinner.frame = CGRectMake(0.0, 0.0, 40.0, 40.0)
+        self.spinner.center = CGPointMake(self.connectButton.frame.size.width/2.0, self.connectButton.frame.size.height/2)
+        self.connectButton.addSubview(spinner)
         
         if PFUser.currentUser() {
             //self.performSegueWithIdentifier("loggedIn", sender: self)
@@ -18,14 +27,18 @@ class LandingViewController: UIViewController {
     }
     
     @IBAction func connectButtonDown(sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 0.14, green: 0.22, blue: 0.36, alpha: 1)
+        self.connectButton.backgroundColor = UIColor(red: 0.14, green: 0.22, blue: 0.36, alpha: 1)
     }
     
     @IBAction func connectButtonReleased(sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 0.25, green: 0.37, blue: 0.58, alpha: 1)
+        self.connectButton.backgroundColor = UIColor(red: 0.25, green: 0.37, blue: 0.58, alpha: 1)
+        self.connectButton.setTitleColor(UIColor.clearColor(), forState: UIControlState.Normal)
+        self.spinner.startAnimating()
         
-        PFFacebookUtils.logInWithPermissions(nil, {
-            (user: PFUser!, error: NSError!) -> Void in
+        PFFacebookUtils.logInWithPermissions(nil, { (user: PFUser!, error: NSError!) -> Void in
+            self.connectButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            self.spinner.stopAnimating()
+            
             if !user {
                 NSLog("Uh oh. The user cancelled the Facebook login.")
             } else if user.isNew {
