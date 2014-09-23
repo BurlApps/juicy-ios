0fd8e9e601e2e861602899fec4642bbb3484bb9c
Brian Vallelunga <vallelungabrian@gmail.com>
2014-09-23 00:44:08 -0700

    removed unneeded comment

--- Juicy/String.swift
+++ Juicy/String.swift
@@ -13,25 +13,3 @@ extension String {
         return String(Array(self)[i])
     }
 }
-
-//-(NSString*) suffixNumber:(NSNumber*)number
-//{
-//    if (!number)
-//    return @"";
-//    
-//    long long num = [number longLongValue];
-//    
-//    int s = ( (num < 0) ? -1 : (num > 0) ? 1 : 0 );
-//    NSString* sign = (s == -1 ? @"-" : @"" );
-//    
-//    num = llabs(num);
-//    
-//    if (num < 1000)
-//    return [NSString stringWithFormat:@"%@%lld",sign,num];
-//    
-//    int exp = (int) (log(num) / log(1000));
-//    
-//    NSArray* units = @[@"K",@"M",@"G",@"T",@"P",@"E"];
-//    
-//    return [NSString stringWithFormat:@"%@%.1f%@",sign, (num / pow(1000, exp)), [units objectAtIndex:(exp-1)]];
-//}
